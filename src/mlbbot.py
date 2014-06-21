8b9724ce5ac5883c94b800c5cd8dd1c7e90afa4d
Joshua Wertheim <joshwertheim@gmail.com>
2014-06-20 20:22:33 -0700

    got story blurbs working

--- src/mlbbot.py
+++ src/mlbbot.py
@@ -4,41 +4,36 @@
 import sys
 import os
 import time
+from datetime import date
 import urllib2
 import json
 
-input_tag = raw_input("Enter a tag... ")
-print("Tag entered: " + input_tag)
+# d = date.today()
+# today = d.timetuple()
 
-tag = input_tag #"quote"
-api_key = "YDwTx7EpF5KH2ffxeMpE70GQQk4JLsIsrCQkURMQ5u9hrJJniY"
+headlinesUrl = "http://sanfrancisco.giants.mlb.com/gen/sf/news/headlines.json"
 
-response = urllib2.urlopen("http://api.tumblr.com/v2/tagged?tag=" + tag + "&api_key=" + api_key)
-data = json.load(response) 
+responseHeadline = urllib2.urlopen(headlinesUrl)
+data = json.load(responseHeadline) 
 data = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')) # pretty printed json file object data
-
-with open('data.json', 'w') as outfile: # save json response file object to data.json
-    outfile.write(data)
-response.close()
 
 # print data
 
 loadedJSON = json.loads(data)
-length = len(loadedJSON["response"])
+length = len(loadedJSON["members"])
 
-listOfQuotes = []
-allTags = []
+storyElements = []
+storyBlurbs = []
 
 for index in range(length):
-    allTags.append(loadedJSON["response"][index]["tags"])
-    if "text" in loadedJSON["response"][index]:
-        # print loadedJSON["response"][index]["text"]
-        listOfQuotes.append(loadedJSON["response"][index]["text"])
-    elif "body" in loadedJSON["response"][index]:
-        # print loadedJSON["response"][index]["body"]
-        listOfQuotes.append(loadedJSON["response"][index]["body"])
+    storyElements.append(loadedJSON["members"][index])
+    # if "text" in loadedJSON["response"][index]:
+    #     # print loadedJSON["response"][index]["text"]
+    #     listOfQuotes.append(loadedJSON["response"][index]["text"])
+    # elif "body" in loadedJSON["response"][index]:
+    #     # print loadedJSON["response"][index]["body"]
+    #     listOfQuotes.append(loadedJSON["response"][index]["body"])
 
-os.system('cls' if os.name=='nt' else 'clear')
-
-for quote in range(len(listOfQuotes)):
-    print listOfQuotes[quote] + "\n"
+for index in range(length):
+    storyBlurbs.append(storyElements[index]["althead"])
+    print storyBlurbs[index]
